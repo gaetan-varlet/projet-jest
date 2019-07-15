50888c215613e6551ef28fb50bc507def889bf3d
Gaëtan Varlet <gaetan.varlet@insee.fr>
2019-07-15 08:04:40 +0200

    corrections

--- math.test.js
+++ math.test.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-undef */
 import sum from './math'
 
-// eslint-disable-next-line no-undef
 test('add 1 + 3 to equal 3', () => {
-    expect(sum(1, 2)).toBe(4)
+    expect(sum(1, 2)).toBe(3)
 })
